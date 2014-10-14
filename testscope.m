7b66ee2be97dfac02ce7432363f5560289b9a268
Sharad Shanbhag <sshanbhag@gmail.com>
2014-10-14 19:00:16 -0400

    added spectrogram display.

--- testscope.m
+++ testscope.m
@@ -2,7 +2,7 @@ clear all
 
 fs = 500000;
 NFFT = 1024;
-T = [18 1];
+T = [10 5];
 
 
 
@@ -11,24 +11,7 @@ s = chirp(t, 1, max(t), fs/2);
 N  = length(s); 
  
 figure(1)
-subplot(211)
 tic
 a = myspectrogram(s, fs, T, @hamming, NFFT, [-100 -1], false, 'default', false, 'per');
 toc
 
-subplot(212)
-Tw = T(1);                          % frame width (ms)
-Ts = T(2);                          % frame shift (ms)
-Nw = round(fs*Tw*0.001);            % frame width (samples)
-Ns = round(fs*Ts*0.001);            % frame shift (samples)
-w = hamming(Nw).';
-[S,F,T] = spectrogram(s, w(Nw).',Nw-Ns, NFFT,fs);
-S = abs(S);                         % compute magnitude spectrum 
-S = S/max(max(S));                  % normalize magntide spectrum
-S = 20*log10(S);                    % compute power spectrum in dB
-
-
-handle = imagesc(T, F, S, [Smin Smax]);
-%handle = imagesc(T, F, S, 'CDataMapping', 'direct');
-axis('xy');
-axis([0 N/fs  0 fs/2]);
