c51a3fba4764c9c4759609c1e031653b427ea75e
Sharad Shanbhag <sshanbhag@gmail.com>
2017-09-15 12:21:50 -0400

    added grid

--- PlotSweepData.m
+++ PlotSweepData.m
@@ -20,16 +20,20 @@ plot(tvec_acq, Lacq);
 title('Left', 'Color', 'g');
 xlabel('Time (ms)');
 ylabel('Volts');
+grid('on');
 
 subplot(222)
 plot(tvec_acq, Racq);
 title('Right', 'Color', 'r');
 xlabel('Time (ms)');
+grid('on');
 
 subplot(223)
 plot(fvec, Lfft);
 xlabel('Frequency (kHz)');
+grid('on');
 
 subplot(224)
 plot(fvec, Rfft);
 xlabel('Frequency (kHz)');
+grid('on');
